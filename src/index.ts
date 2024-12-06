adbd508e46f2388164e07737efe81504bc66eddb
hanzalahwaheed <hanzalah.w@gmail.com>
2024-12-07 01:05:49 +0530

    initialise repo

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
